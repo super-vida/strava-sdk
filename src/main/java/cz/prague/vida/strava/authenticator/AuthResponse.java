6b9dc2d9e34f53d4ef351bedf3fd4d228b3f87d9
vacek <jvacek@gpe.cz>
2023-11-30 10:08:47 +0100

    Lombok added

--- src/main/java/cz/prague/vida/strava/authenticator/AuthResponse.java
+++ src/main/java/cz/prague/vida/strava/authenticator/AuthResponse.java
@@ -1,35 +1,14 @@
 package cz.prague.vida.strava.authenticator;
 
-
 import com.google.gson.annotations.SerializedName;
 import cz.prague.vida.strava.entities.Athlete;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
 public class AuthResponse {
-    @SerializedName("access_token")
-    String accessToken;
-    Athlete athlete;
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public Athlete getAthlete() {
-        return athlete;
-    }
-
-    public void setAthlete(Athlete athlete) {
-        this.athlete = athlete;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthResponse{" +
-                "accessToken='" + accessToken + '\'' +
-                ", athlete=" + athlete +
-                '}';
-    }
+	@SerializedName("access_token")
+	String accessToken;
+	Athlete athlete;
 }
--- src/main/java/cz/prague/vida/strava/authenticator/RefreshTokenResponse.java
+++ src/main/java/cz/prague/vida/strava/authenticator/RefreshTokenResponse.java
@@ -1,7 +1,9 @@
 package cz.prague.vida.strava.authenticator;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.Data;
 
+@Data
 public class RefreshTokenResponse {
 	@SerializedName("token_type")
 	private String tokenType;
@@ -14,43 +16,4 @@ public class RefreshTokenResponse {
 	@SerializedName("refresh_token")
 	private String refreshToken;
 
-	public String getTokenType() {
-		return tokenType;
-	}
-
-	public void setTokenType(String tokenType) {
-		this.tokenType = tokenType;
-	}
-
-	public String getAccessToken() {
-		return accessToken;
-	}
-
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
-
-	public int getExpiresAt() {
-		return expiresAt;
-	}
-
-	public void setExpiresAt(int expiresAt) {
-		this.expiresAt = expiresAt;
-	}
-
-	public int getExpiresIn() {
-		return expiresIn;
-	}
-
-	public void setExpiresIn(int expiresIn) {
-		this.expiresIn = expiresIn;
-	}
-
-	public String getRefreshToken() {
-		return refreshToken;
-	}
-
-	public void setRefreshToken(String refreshToken) {
-		this.refreshToken = refreshToken;
-	}
 }
